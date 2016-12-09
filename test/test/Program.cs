b18c9a319b369ebeeced5034ec59693c15c55780
Nikita Kurilev <n.kurilev@gmail.com>
2016-12-09 19:48:43 +0300

    More useful(?) methods

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -12,24 +12,36 @@ namespace test
 {
     class Program
     {
+        static void TextToColor(string name)
+        {
+            switch (name)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+        }
         public class Exec
         {
+            public void methodlist(string arg)
+            {
+                Type type = typeof(Exec);
+                foreach (var method in type.GetMethods())
+                {
+                    Console.WriteLine(method.Name);
+                }
+            }
             public void intro(string arg)
             {
                 String[] slogan = new string[5] { "Why am I here?", "You should go for a walk", "I bet you have other things to do", "wow much text so console", "What have I done..." };
                 Random rand = new Random();
-                switch (arg.ToLower())
-                {
-                    case "red":
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    case "blue":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        break;
-                    case "green":
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                }
+                TextToColor(arg.ToLower());
                 Console.WriteLine("\n" +
                               "      _______ _____  _____ _______\n" +
                               "     /__  __// ___/ / ___//__  __/\n" +
@@ -41,48 +53,52 @@ namespace test
             }
             public void color(string arg)
             {
-                switch (arg.ToLower())
-                {
-                    case "red":
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    case "blue":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        break;
-                    case "green":
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                }
+                TextToColor(arg.ToLower());
             }
-        }
-
-        public static void Main(string[] args)
-        {
-            Exec exec = new Exec();
-            exec.intro("");
-            // TODO: Implement Functionality Here
-            while (true)
+            public void clear(string arg)
             {
-                Console.Write("\n>");
-                string s = Console.ReadLine() + " ";
-                if (s != " ")
-                {
-                    Type type = typeof(Exec);
-                    MethodInfo method = type.GetMethod(s.Split(' ')[0]);
-                    Exec c = new Exec();
-                    try
-                    {
-                        method.Invoke(c, new Object[1] { s.Split(' ')[1] });
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Invalid request");
-                    }
-                }
-
+                Console.Clear();
             }
-            Console.Write("\n     Press any key to continue . . . ");
-            Console.ReadKey(true);
+            public void echo(string arg)
+            {
+                try
+                {
+                    for (int i = 0; i < Convert.ToInt32(arg.Split('*')[1]); i++)
+                        Console.WriteLine(arg.Split('*')[0]);
+                }
+                catch
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+            public static void Main(string[] args)
+            {
+                Exec exec = new Exec();
+                exec.intro("");
+                // TODO: Implement Functionality Here
+                while (true)
+                {
+                    Console.Write("\n>");
+                    string s = Console.ReadLine() + " ";
+                    if (s != " ")
+                    {
+                        Type type = typeof(Exec);
+                        MethodInfo method = type.GetMethod(s.Split(' ')[0]);
+                        Exec c = new Exec();
+                        try
+                        {
+                            method.Invoke(c, new Object[1] { s.Split(' ')[1] });
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid request");
+                        }
+                    }
+
+                }
+                Console.Write("\n     Press any key to continue . . . ");
+                Console.ReadKey(true);
+            }
         }
     }
 }
